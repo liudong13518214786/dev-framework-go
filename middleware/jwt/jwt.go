64cac0e23e3069fe4ca1c4edca9ba1ad83020ca8
liudong <200826>
2019-12-03 18:30:19 +0800

    1

--- middleware/jwt/jwt.go
+++ middleware/jwt/jwt.go
@@ -7,6 +7,21 @@ import (
 	"net/http"
 )
 
+/*
+iss：发行人
+exp：到期时间
+sub：主题
+aud：用户
+nbf：在此之前不可用
+iat：发布时间
+jti：JWT ID用于标识该JWT
+除以上默认字段外，我们还可以自定义私有字段，如下例：
+{
+"sub": "1234567890",
+"name": "chongchong",
+"admin": true
+}
+*/
 func JWT() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.Query("token")
