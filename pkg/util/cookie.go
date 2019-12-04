d19bc940a7a43ac96ebb46d67760b0e838c4c31f
liudong <200826>
2019-12-04 14:58:52 +0800

    1

---
+++ pkg/util/cookie.go
@@ -0,0 +1,67 @@
+package util
+
+import (
+	"dev-framework-go/conf"
+	"encoding/base64"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/wonderivan/logger"
+	"strings"
+)
+
+type CookieManger struct {
+	C           *gin.Context
+	SessionName string
+	ExpireTime  int
+}
+
+func (s *CookieManger) SetSecureCookie(sessionId string) {
+	secureCookie := createSignedValue(s.SessionName, sessionId)
+	s.C.SetCookie(s.SessionName, secureCookie, s.ExpireTime, "/", conf.DOMAIN, false, true)
+}
+
+func (s *CookieManger) GetSessionid() string {
+	sessionId := s.GetSecureCookie()
+	if sessionId == "" {
+		sessionId = CreatSessionId()
+	}
+	return sessionId
+}
+
+func (s *CookieManger) GetSecureCookie() string {
+	res, err := s.C.Cookie(conf.SESSION_NAME)
+	if err != nil {
+		//没有获取到，为登陆状态
+		return ""
+	}
+	sessionId := decodeSignedValue(res)
+	return sessionId
+}
+
+func CreatSessionId() string {
+	uuix := uuid.New()
+	sessionId := strings.Replace(uuix.String(), "-", "", -1)
+	return sessionId
+}
+
+func decodeSignedValue(sign string) string {
+	res, err := base64.StdEncoding.DecodeString(sign)
+	if err != nil {
+		logger.Error("[BASE64 DECODE ERROR]sign=>%s", sign)
+		return ""
+	}
+	result := strings.Split(string(res), ":")
+	if len(result) != 3 {
+		logger.Error("[COOIKE ERROR]300")
+		return ""
+	}
+	if result[2] != conf.COOKIE_SECRET {
+		logger.Error("[COOIKE ERROR]301")
+	}
+	return result[1]
+}
+
+func createSignedValue(name, value string) string {
+	sign := []byte(name + ":" + value + ":" + conf.COOKIE_SECRET)
+	return base64.StdEncoding.EncodeToString(sign)
+}
--- pkg/util/session.go
+++ pkg/util/session.go
@@ -1,67 +1,16 @@
 package util
 
-import (
-	"dev-framework-go/conf"
-	"encoding/base64"
-	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
-	"github.com/wonderivan/logger"
-	"strings"
-)
-
-type CookieManger struct {
-	C           *gin.Context
-	SessionName string
-	ExpireTime  int
+type SessionManager struct {
 }
 
-func (s *CookieManger) SetSecureCookie(sessionId string) {
-	secureCookie := createSignedValue(s.SessionName, sessionId)
-	s.C.SetCookie(s.SessionName, secureCookie, s.ExpireTime, "/", conf.DOMAIN, false, true)
+func (s *SessionManager) Set() {
+
 }
 
-func (s *CookieManger) GetSessionid() string {
-	sessionId := s.GetSecureCookie()
-	if sessionId == "" {
-		sessionId = CreatSessionId()
-	}
-	return sessionId
+func (s *SessionManager) Get() {
+
 }
 
-func (s *CookieManger) GetSecureCookie() string {
-	res, err := s.C.Cookie(conf.SESSION_NAME)
-	if err != nil {
-		//没有获取到，为登陆状态
-		return ""
-	}
-	sessionId := decodeSignedValue(res)
-	return sessionId
-}
+func (s *SessionManager) Del() {
 
-func CreatSessionId() string {
-	uuix := uuid.New()
-	sessionId := strings.Replace(uuix.String(), "-", "", -1)
-	return sessionId
-}
-
-func decodeSignedValue(sign string) string {
-	res, err := base64.StdEncoding.DecodeString(sign)
-	if err != nil {
-		logger.Error("[BASE64 DECODE ERROR]sign=>%s", sign)
-		return ""
-	}
-	result := strings.Split(string(res), ":")
-	if len(result) != 3 {
-		logger.Error("[COOIKE ERROR]300")
-		return ""
-	}
-	if result[2] != conf.COOKIE_SECRET {
-		logger.Error("[COOIKE ERROR]301")
-	}
-	return result[1]
-}
-
-func createSignedValue(name, value string) string {
-	sign := []byte(name + ":" + value + ":" + conf.COOKIE_SECRET)
-	return base64.StdEncoding.EncodeToString(sign)
 }
