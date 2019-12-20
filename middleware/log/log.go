e4181ef822d59223c731a4ccdc597c2fca20bbee
liudong <200826>
2019-12-20 16:17:50 +0800

    1

--- middleware/log/log.go
+++ middleware/log/log.go
@@ -4,13 +4,16 @@ import (
 	"dev-framework-go/conf"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"strings"
 )
 
 //日志中间件
 func DiyLogger() gin.HandlerFunc {
 	// 请求日志
 	return gin.LoggerWithFormatter(func(p gin.LogFormatterParams) string {
-
+		if strings.HasPrefix(p.Path, "/swagger/") == true {
+			return ""
+		}
 		return fmt.Sprintf("[%s] %d %s %s (%s) [%v] %s\n",
 			p.TimeStamp.Format(conf.TIME_FORMAT),
 			p.StatusCode,
