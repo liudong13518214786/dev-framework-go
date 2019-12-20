26fe3300f8963c340cf435f9fd399bf05a8f6375
liudong <200826>
2019-12-20 18:23:34 +0800

    1

--- pkg/session/session.go
+++ pkg/session/session.go
@@ -6,6 +6,22 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+//定义session
+type Session interface {
+	Set(key, value interface{}) error
+	Get(key interface{}) interface{}
+	Delete(key interface{}) error
+	SessionId() string
+}
+
+//session管理器
+type SessionManager interface {
+	SessionInit(sid string) (Session, error)
+	SessionRead(sid string) (Session, error)
+	SessionDestroy(sid string) error
+	SessionGC(maxLifeTime int64)
+}
+
 func SessionGet(c *gin.Context, key string) string {
 	cookieManger := &util.CookieManger{C: c, SessionName: conf.SESSION_NAME, ExpireTime: conf.COOKIE_EXPIRE_TIME}
 	cookieId := cookieManger.GetSessionid()
