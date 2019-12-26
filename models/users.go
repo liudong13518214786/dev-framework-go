7ab26e90094b67ed71807ef37b2274bcbf9e3eb0
liudong <ld954120liudong>
2019-12-26 20:31:40 +0800

    1

--- models/users.go
+++ models/users.go
@@ -3,7 +3,6 @@ package models
 import (
 	"dev-framework-go/pkg/cache"
 	"dev-framework-go/pkg/db"
-	"fmt"
 )
 
 type Users struct {
@@ -17,11 +16,9 @@ type Users struct {
 func GetUserInfoByTel(tel string) Users {
 	var u Users
 	xx := cache.GetKey("userinfo")
-	fmt.Println(xx)
 	if xx != nil {
 		return xx.(Users)
 	}
-
 	db.DBPool.Table("users").Select("uuid,email,real_name,tel,status").Where("tel=?", tel).First(&u)
 	cache.SetKey("userinfo", u, 10)
 	return u
