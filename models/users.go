ebddc331b9d0080a4f0509c8dc688e86d60f1e13
liudong <200826>
2019-12-27 10:24:37 +0800

    1

--- models/users.go
+++ models/users.go
@@ -3,6 +3,7 @@ package models
 import (
 	"dev-framework-go/pkg/cache"
 	"dev-framework-go/pkg/db"
+	"encoding/json"
 )
 
 type Users struct {
@@ -17,9 +18,11 @@ func GetUserInfoByTel(tel string) Users {
 	var u Users
 	xx := cache.GetKey("userinfo")
 	if xx != nil {
-		return xx.(Users)
+		_ = json.Unmarshal([]byte(xx.(string)), &u)
+		return u
 	}
 	db.DBPool.Table("users").Select("uuid,email,real_name,tel,status").Where("tel=?", tel).First(&u)
-	cache.SetKey("userinfo", u, 10)
+	jsons, _ := json.Marshal(u)
+	cache.SetKey("userinfo", string(jsons), 10)
 	return u
 }
--- test.go
+++ test.go
@@ -10,13 +10,27 @@ func calc(index string, a, b int) int {
 	return ret
 }
 
+type Users struct {
+	Name string
+}
+
 func main() {
-	x := 1
-	y := 2
-	tp1 := calc("B", x, y)
-	defer calc("A", x, tp1)
-	x = 3
-	tp2 := calc("D", x, y)
-	defer calc("C", x, tp2)
-	y = 4
+	//x := 1
+	//y := 2
+	//tp1 := calc("B", x, y)
+	//defer calc("A", x, tp1)
+	//x = 3
+	//tp2 := calc("D", x, y)
+	//defer calc("C", x, tp2)
+	//y = 4
+	var b Users
+	b = Users{"123"}
+	fmt.Println(b)
+	xx(b)
+}
+
+func xx(x interface{}) {
+	a, e := x.(Users)
+	fmt.Println(e)
+	fmt.Println(a)
 }
