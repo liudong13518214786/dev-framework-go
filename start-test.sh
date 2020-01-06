522ec412b4d3392e8aba568934abb5e155d12dec
liudong <200826>
2020-01-06 15:33:42 +0800

    1

--- start-test.sh
+++ start-test.sh
@@ -10,5 +10,5 @@ git pull
 echo "update frontend code success!"
 echo "start run docker-compose"
 cd /Users/liudong/go/src/dev-framework-go/
-docker-compose -f docker-compose-prod.yml up -d
+docker-compose -f docker-compose.yml up -d
 echo "server run success! http://127.0.0.1:8100"
--- start.sh
+++ start.sh
@@ -10,5 +10,5 @@ git pull
 echo "update frontend code success!"
 echo "start run docker-compose"
 cd ../dev-framework-go/
-docker-compose -f docker-compose.yml up -d
+docker-compose -f docker-compose-prod.yml up -d
 echo "server run success! http://47.93.19.60"
